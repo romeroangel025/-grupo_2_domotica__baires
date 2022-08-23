ddc3601aa2be4114067352b4c596de52205b75b1
Angel <romeroangel025@gmail.com>
2022-08-23 20:32:37 -0300

    CRUD

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,24 +1,87 @@
+const fs = require('fs');
+const path = require('path');
+const { loadProducts,storeProducts } = require('../data/productsFunction.js');
+
+const productsFilePath = path.join(__dirname, '../data/DataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
 
 module.exports = {
-    detail: (req, res) => {
-      return res.render("productDetail", {
-        title: "detalle"
-      
-      });
-    },
+    
     add:(req, res) => {
       return res.render("productAdd", {
         title: "añadir producto"
       
       });
 
+
     },
+    // Create -  Method to store
+	store: (req, res) => {
+		// Do the magic
+		const { id } = req.params;
+		let { nombre, precio, descuento, descripcion, categoria} = req.body;
+		//const imagen = req.files["image"] ;
+		let newProduct = {
+		  id: products[products.length - 1].id + 1,
+		  nombre: nombre.trim(),
+		  descripcion: descripcion.trim(),
+		  precio: +precio,
+		  descuento: +descuento,
+		  categoria:categoria,
+		  image: "default-image.png",
+		};
+	
+		let productsNew = [...products, newProduct];
+	
+		storeProducts(productsNew);
+		return res.redirect("/");
+	},
     edit:(req, res) => {
+
+      const product = products.find(product => product.id===+req.params.id);
+
+
       return res.render("productEdit", {
-        title: "eliminar producto"
+        title: "editar producto",product
       
       });
 
     },
+    // Update - Method to update
+	update: (req, res) => {
+		//do the magic
+
+		const { id } = req.params;
+		let { nombre, precio, descuento, descripcion, categoria } = req.body;
+	
+		const productModify = products.map((product) => {
+		  if (product.id === +id) {
+			return {
+			  ...product,
+			  nombre: nombre.trim(),
+			  descripcion: descripcion.trim(),
+			  precio: +precio,
+			  descuento: +descuento,
+			  categoria:categoria,
+			};
+		  } else {
+			return product;
+		  }
+		});
+	
+		storeProducts(productModify);
+		return res.redirect("/products/detail/" + id);
+	  },
+    // Delete - Delete one product from DB
+	destroy : (req, res) => {
+		const {id} = req.params;
+        const products = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'data', 'DataBase.json')));
+
+        const productFilter = products.filter(product => product.id !== +id);
+
+        storeProducts(productFilter);
+        return res.redirect('/');
+  }
   }
       
--- routes/products.js
+++ routes/products.js
@@ -1,12 +1,22 @@
 var express = require('express');
 
 var router = express.Router();
-const {detail,add,edit } = require('../controllers/productsController');
+const {add,edit,store,destroy,update} = require('../controllers/productsController');
+const {detail} = require('../controllers/detailControllers')
+
 
 /* GET users listing. */
-router.get('/detail/:id',detail );
-router.get('/productAdd/:id',add);
-router.get('/productEdit/:id',edit);
-  
+router.get('/detail/:id', detail );
+
+/*** CREATE ONE PRODUCT ***/ 
+router.get('/productAdd', add);
+router.post('/store', store); 
+
+/*** EDIT ONE PRODUCT ***/ 
+router.get('/edit/:id',edit); 
+router.put('/edit/:id',update); 
+
+/*** DELETE ONE PRODUCT***/ 
+router.delete('/delete/:id', destroy); 
 
 module.exports = router;
