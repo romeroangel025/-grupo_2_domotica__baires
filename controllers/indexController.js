77a59e32819c94d6a78b57398da32c1d97ff217d
JuanAlarcon123 <grabados.todoenvidrios@gmail.com>
2022-08-23 15:50:38 -0300

    index dinamico

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,9 +1,17 @@
 
+const products = require('../data/dataBase.json')
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 module.exports = {
   index: (req, res) => {
+    let oferta = products.filter(product => product.categoria === 'oferta')
+    let destacado = products.filter(product => product.categoria === 'destacado')
+
     return res.render("index", {
-      title: "Home"
+      title: "Home",oferta,
+      destacado,toThousand
+            
+
     
     });
   },
