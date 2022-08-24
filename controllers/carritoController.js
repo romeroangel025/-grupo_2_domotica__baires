4ef025dddab962f1e3dfa7d35df330a259943f4b
MaxiOrellanadev <maxiorellana.bru@gmail.com>
2022-08-23 21:52:41 -0300

    carrito listo

--- controllers/carritoController.js
+++ controllers/carritoController.js
@@ -1,8 +1,9 @@
-
+const products = require('../data/dataBase.json');
 module.exports = {
     carrito: (req, res) => {
+      let carrito = products.filter(product => product.categoria === 'carrito')
       return res.render("productCart", {
-        title: "Carrito"
+        title: "Carrito", carrito
       
       });
     },
