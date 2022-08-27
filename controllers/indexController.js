0493e3154e31720bcd18ef9244e8a00581d70e7b
JuanAlarcon123 <grabados.todoenvidrios@gmail.com>
2022-08-26 23:01:08 -0300

    acentos del search solucionado

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,6 +1,18 @@
 
 const products = require('../data/dataBase.json')
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const removeAccents = str =>  str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+/* 
+    let busqueda =  removeAccents(req.query.busqueda);
+
+    let result = products.filter (product => removeAccents(product.nombre.toLowerCase()).includes(busqueda.toLowerCase()))
+    return res.render('results',{
+      title : 'Resultado de Busqueda',
+      products: result , 
+      busqueda,
+      toThousand
+
+    }) */
 
 module.exports = {
   index: (req, res) => {
@@ -13,9 +25,13 @@ module.exports = {
     });
   },
   search:(req,res) => {
-    let busqueda =  req.query.busqueda
-
+    let busqueda =  removeAccents(req.query.busqueda)
+/*     let busqueda =  req.query.busqueda
     let result = products.filter (product => product.nombre.toLowerCase().includes(busqueda.toLowerCase()))
+
+ */
+    let result = products.filter (product => removeAccents(product.nombre.toLowerCase()).includes(busqueda.toLowerCase()))
+
     return res.render('results',{
       title : 'Resultado de Busqueda',
       products: result , 
