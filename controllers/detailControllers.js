3097d36ede02ce5e24bcffddda66088830804f78
Angel <romeroangel025@gmail.com>
2022-10-03 16:05:18 -0300

    products

--- controllers/detailControllers.js
+++ controllers/detailControllers.js
@@ -1,15 +1,13 @@
 const fs = require('fs');
 const path = require('path');
-const productos = require('../data/dataBase.json');
 const { loadProducts,storeProducts } = require('../data/productsFunction');
-const productsFilePath = path.join(__dirname, '../data/dataBase.json');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
 
 module.exports = {
     detail: (req, res) => {
       const products=loadProducts();
 
-      let producto = productos.find(producto => producto.id === +req.params.id)
+      let producto = products.find(producto => producto.id === +req.params.id)
 
       return res.render("productDetail", {
         title: "detalle",
