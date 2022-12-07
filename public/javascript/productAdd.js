2386db8d35548a743f0cd2105eaa6a5524cb360f
JuliSchegtel <julieta.schegtel@gmail.com>
2022-12-07 07:18:32 -0300

    Actualizando product js

--- public/javascript/productAdd.js
+++ public/javascript/productAdd.js
@@ -2,4 +2,45 @@ console.log('productAdd connected success!');
 
 const formProductAdd = $('formProductAdd');
 
-console.log(formProductAdd);
+const elements = formProductAdd.elements;
+
+$('name').addEventListener('focus', function(e){
+
+    $('nameMsg').innerHTML = "Este campo no puede estar en blanco";
+
+});
+
+$('name').addEventListener('blur', function(e){
+
+    $('nameMsg').innerHTML = null;
+});
+
+$('price').addEventListener('focus', function(e){
+
+    $('priceMsg').innerHTML = "Mínimo 0";
+
+});
+
+$('price').addEventListener('blur', function(e){
+
+    $('priceMsg').innerHTML = null;
+});
+
+$('price').addEventListener('keyup', function(e){
+
+    let price = this.value;
+    let discount = $('discount').value;
+
+$('finalPrice').value = +price - (+price * +discount / 100)
+});
+
+$('discount').addEventListener('keyup', function(e){
+
+    let price = $('price').value;
+    let discount = this.value;
+
+$('finalPrice').value = +price - (+price * +discount / 100)
+});
+
+
+
