68a1da53887b50fab12ce75ebea7a794689aa680
Angel <romeroangel025@gmail.com>
2022-12-09 03:14:05 -0300

    api products all

--- src/controllers/APIs/productsController.js
+++ src/controllers/APIs/productsController.js
@@ -4,6 +4,8 @@ const { Op } = require("sequelize");
 const fs = require("fs");
 
 const { sendJsonError } = require("../../helpers/sendJsonError");
+const { literalQueryUrlImage } = require("../../helpers/literalQueryUrlImage");
+
 sendJsonError;
 const controller = {
   image: (req, res) => {
@@ -16,18 +18,60 @@ const controller = {
   },
   all: async (req, res) => {
     try {
+      let { page = 1, limit = 5, offset = 0 } = req.query;
 
-const {page = 1 ,limit = 10 ,offset = 0} = req.query
+      limit = +limit > 10 ? 10 : +limit;
 
+      page = +page <= 0 || isNaN(+page) ? 1 : +page;
 
+      page -= 1;
 
-      const products = await db.Product.findAll();
-      return res.status(200).json({
-        ok: true,
-        status: 200,
-        data: products,
+      offset = page * limit;
+
+      const { count, rows: products } = await db.Product.findAndCountAll({
+        limit,
+        offset,
+        include: [
+          {
+            association: "images",
+            attributes: {
+              include: [
+                literalQueryUrlImage(req, "images.name", "urlImage"), //preguntar por los parametros
+              ],
+              exclude: ["updatedAt", "createdAt", "deletedAt"],
+            },
+          },
+        ],
+        attributes: {
+          exclude: ["updatedAt", "deletedAt"],
+        },
       });
 
+const existPrev = page>0 && offset<= count
+const existNext = Math.floor(count / limit )>=( page+1 )&& limit!=count;
+
+let urlPrev = null;
+let urlNext = null;
+if (existNext) {
+    urlNext = `${req.protocol}://${req.get("host")}${req.baseUrl}?page=${page+2}`
+}
+
+if (existPrev) {
+    urlPrev = `${req.protocol}://${req.get("host")}${req.baseUrl}?page=${page}`
+}
+
+      return res.status(200).json({
+        meta: {
+          ok: true,
+          status: 200,
+        },
+        data: {
+          totalProducts: count,
+          prev: urlPrev,
+          next: urlNext,
+          products,
+        },
+      });
     } catch (error) {
       sendJsonError(error, res);
     }
