579895228a5afa85694a845409559f5f39d5ceae
Angel <romeroangel025@gmail.com>
2022-12-11 00:55:10 -0300

    api products all

--- src/controllers/APIs/productsController.js
+++ src/controllers/APIs/productsController.js
@@ -16,19 +16,46 @@ const controller = {
       )
     );
   },
+  search: (req, res) => {},
+
   all: async (req, res) => {
     try {
-      let { page = 1, limit = 5, offset = 0 } = req.query;
+      let {
+        limit = 10,
+        sales = 0,
+        order = "ASC",
+        offset = 0,
+        page = 1,
+        salesDiscount = 10,
+        search = "",// no se por que da before declaration acess
+      } = req.query;
+
+      const queriesValuesDefaultAndModify = {
+        limit,
+        order,
+        sales,
+        salesDiscount,
+         search, 
+      };
 
       limit = +limit > 10 ? 10 : +limit;
 
+      salesDiscount = +salesDiscount < 5 ? 5 : +salesDiscount;
+
       page = +page <= 0 || isNaN(+page) ? 1 : +page;
+
+      let urlQueries = "";
+      const queries = queriesValuesDefaultAndModify;
+
+      for (const key in queries) {
+        urlQueries += `&${key}=${queries[key]}`;
+      }
 
       page -= 1;
 
       offset = page * limit;
 
-      const { count, rows: products } = await db.Product.findAndCountAll({
+      let options = {
         limit,
         offset,
         include: [
@@ -45,20 +72,60 @@ const controller = {
         attributes: {
           exclude: ["updatedAt", "deletedAt"],
         },
-      });
+        order: [["title", order]],
+        where: {
+          [Op.or]: [
+            {
+              title: {
+                [Op.substring]: search,
+              },
+            },
+          ],
+        },
+      };
 
-const existPrev = page>0 && offset<= count
-const existNext = Math.floor(count / limit )>=( page+1 )&& limit!=count;
+      const optionsSales = {
+        ...options,
+        where: {
+          discount: {
+            [Op.gte]: salesDiscount,
+          },
+        },
+      };
 
-let urlPrev = null;
-let urlNext = null;
-if (existNext) {
-    urlNext = `${req.protocol}://${req.get("host")}${req.baseUrl}?page=${page+2}`
+      if (+sales == 1 && !isNaN(sales)) {
+        options = optionsSales;
+      }
+
+      const { count, rows: products } = await db.Product.findAndCountAll(
+        options
+      );
+
+if(!products.length){
+    return res.status(200).json({
+        ok:true,
+        status:204,
+        message:"No hay productos en esta pagina"
+    })
 }
 
-if (existPrev) {
-    urlPrev = `${req.protocol}://${req.get("host")}${req.baseUrl}?page=${page}`
-}
+
+      const existPrev = page > 0 && offset <= count;
+      const existNext = Math.floor(count / limit) >= page + 1 && limit != count;
+
+      let urlPrev = null;
+      let urlNext = null;
+      if (existNext) {
+        urlNext = `${req.protocol}://${req.get("host")}${req.baseUrl}?page=${
+          page + 2
+        }${urlQueries}`;
+      }
+
+      if (existPrev) {
+        urlPrev = `${req.protocol}://${req.get("host")}${
+          req.baseUrl
+        }?page=${page}`;
+      }
 
       return res.status(200).json({
         meta: {
