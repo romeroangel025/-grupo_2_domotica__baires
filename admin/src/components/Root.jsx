2fad2612114cb92984e292313e2c63c809346666
Eric <menaericdaniel@gmail.com>
2022-12-14 14:31:18 -0300

    fix start react dev

--- admin/src/components/Root.jsx
+++ admin/src/components/Root.jsx
@@ -164,7 +164,7 @@ export const Root = () => {
 								</div>
 								<div class="card-body">
 									<div class="text-center">
-										<img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 40rem;" src="assets/images/mandalorian.jpg" alt=" Star Wars - Mandalorian "/>
+										<img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: "40rem;"}} src="assets/images/mandalorian.jpg" alt=" Star Wars - Mandalorian "/>
 									</div>
 									<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, consequatur explicabo officia inventore libero veritatis iure voluptate reiciendis a magnam, vitae, aperiam voluptatum non corporis quae dolorem culpa citationem ratione aperiam voluptatum non corporis ratione aperiam voluptatum quae dolorem culpa ratione aperiam voluptatum?</p>
 									<a class="btn btn-danger" target="_blank" rel="nofollow" href="/">View movie detail</a>
