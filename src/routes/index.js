6fa0925553d579d933f46cbb40efbe079fff3842
Angel <romeroangel025@gmail.com>
2022-12-20 18:10:30 -0300

    link de header

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,13 @@
 var express = require('express');
-const {index,search} = require('../controllers/indexController');
+const {index,search,iluminacion,alarmas,electronica} = require('../controllers/indexController');
 var router = express.Router();
 
 
 /* GET home page. */
 router.get('/', index);
 router.get('/search', search)
+router.get('/iluminacion', iluminacion)
+router.get('/alarmas', alarmas)
+router.get('/electronica', electronica)
 
 module.exports = router;
