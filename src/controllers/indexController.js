2baa168645c41057d1a21e6fb9af8cfb4af6d5ed
Angel <romeroangel025@gmail.com>
2022-12-21 01:47:06 -0300

    index productos db

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,55 +1,45 @@
-
-const { Op } = require('sequelize');
-const products = require('../data/dataBase.json')
-const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-const removeAccents = str =>  str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-const db = require("../database/models")
-/* 
-    let busqueda =  removeAccents(req.query.busqueda);
-
-    let result = products.filter (product => removeAccents(product.nombre.toLowerCase()).includes(busqueda.toLowerCase()))
-    return res.render('results',{
-      title : 'Resultado de Busqueda',
-      products: result , 
-      busqueda,
-      toThousand
-
-    }) */
+const { Op } = require("sequelize");
+const products = require("../data/dataBase.json");
+const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const removeAccents = (str) =>
+  str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+const db = require("../database/models");
 
 module.exports = {
-  index: (req, res) => {
-    let oferta = products.filter(product => product.categoria === 'oferta')
-    let destacado = products.filter(product => product.categoria === 'destacado')
+  index: async (req, res) => {
+    
 
-    return res.render("index", {
-      title: "Home",oferta,
-      destacado,toThousand    
+    let destacados = await db.Product.findAll({
+      include: ["images"],
+      where: {
+        category_id: 2, //categoria iluminacion
+      },
+    });
+
+    let ofertas = await db.Product.findAll({
+      include: ["images"],
+      where: {
+        category_id: 1, //categoria ofertas
+      },
+    });
+
+    res.render("index", {
+      title: "Articulos de iluminacion",
+      destacados,
+      ofertas,
+      toThousand,
     });
   },
-  search:(req,res) => {
- //   let busqueda =  removeAccents(req.query.busqueda)
-     
- 
-  /*   let result = products.filter (product => removeAccents(product.nombre.toLowerCase()).includes(busqueda.toLowerCase()))
-
-    return res.render('results',{
-      title : 'Resultado de Busqueda',
-      products: result , 
-      busqueda,
-      toThousand
-
-    }) */
-    const {busqueda}= req.query;
-    if(busqueda==""){
-        
-        return res.redirect("/")
-    }
-    else{
+  search: (req, res) => {
+    const { busqueda } = req.query;
+    if (busqueda == "") {
+      return res.redirect("/");
+    } else {
       db.Product.findAll({
         include: ["images"],
         where: {
           title: { [Op.like]: `%${busqueda}%` },
-        }
+        },
       }).then((products) =>
         res.render("productlist", {
           title: "Lista de productos",
@@ -59,68 +49,46 @@ module.exports = {
       );
     }
   },
-iluminacion: async (req,res) => {
- 
-  let products = await db.Product.findAll({
-   
-    include: ['images'],
-    where: {
-      category_id: 5,
-    }
-  });
-  
+  iluminacion: async (req, res) => {
+    let products = await db.Product.findAll({
+      include: ["images"],
+      where: {
+        category_id: 5, //categoria iluminacion
+      },
+    });
 
+    res.render("iluminacion", {
+      title: "Articulos de iluminacion",
+      products,
+      toThousand,
+    });
+  },
+  alarmas: async (req, res) => {
+    let products = await db.Product.findAll({
+      include: ["images"],
+      where: {
+        category_id: 4, //categoria  alarmas
+      },
+    });
 
+    res.render("alarmas", {
+      title: "Articulos de alarmas",
+      products,
+      toThousand,
+    });
+  },
+  electronica: async (req, res) => {
+    let products = await db.Product.findAll({
+      include: ["images"],
+      where: {
+        category_id: 6, //categoria electronica
+      },
+    });
 
- res.render("iluminacion", {
-          title: "Articulos de iluminacion",
-          products,
-          toThousand,
-        })
-
-},
-alarmas: async (req,res) => {
-
-  let products = await db.Product.findAll({
-   
-    include: ['images'],
-    where: {
-      category_id: 4,
-    }
-  });
-  
-
-
-
- res.render("alarmas", {
-          title: "Articulos de alarmas",
-          products,
-          toThousand,
-        })
-
- 
-},
-electronica: async (req,res) => {
-
-  let products = await db.Product.findAll({
-   
-    include: ['images'],
-    where: {
-      category_id: 6,
-    }
-  });
-  
-
-
-
- res.render("electronica", {
-          title: "Articulos de electronica",
-          products,
-          toThousand,
-        })
-
-
-
-}
-	
+    res.render("electronica", {
+      title: "Articulos de electronica",
+      products,
+      toThousand,
+    });
+  },
 };
